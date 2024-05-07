422e6bed4ca7e73e18c2c67217962bd810e08cee
HoadreaRazvan <162281824+HoadreaRazvan@users.noreply.github.com>
2024-05-07 13:36:56 +0300

    Terminare Laborator 9

--- Laborator_9/src/test/java/org/example/PhoneBookTest.java
+++ Laborator_9/src/test/java/org/example/PhoneBookTest.java
@@ -3,6 +3,11 @@ package org.example;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PhoneBookTest {
@@ -108,115 +113,231 @@ class PhoneBookTest {
 
     @Test
     void testReadPhoneEntry()  {
-        //Arrage
+        // Arrage
+        String line1 = "Hoadrea Razvan 1234567890 acasa";
+        String line2 = "Hoadrea Bucur 0987654321 serviciu";
 
-        //Act
+        List<PhoneEntry> expectedEntries = new ArrayList<>();
+        expectedEntries.add(new PhoneEntry("Hoadrea", "Razvan", "1234567890", "acasa"));
+        expectedEntries.add(new PhoneEntry("Hoadrea", "Bucur", "0987654321", "serviciu"));
 
-        //Assert
+        // Act
+        List<PhoneEntry> actualEntries = new ArrayList<>();
+        actualEntries.add(pb.parseOneLine(line1));
+        actualEntries.add(pb.parseOneLine(line2));
+
+        // Assert
+        assertEquals(expectedEntries, actualEntries);
     }
 
     @Test
     void testReadPhoneEntryMultipleSpaces()
             throws PhoneAppException {
-        //Arrage
+        // Arrage
+        String line1 = "Hoadrea     Razvan   1234567890      acasa";
+        String line2 = "Hoadrea   Bucur     0987654321   serviciu";
 
-        //Act
+        List<PhoneEntry> expectedEntries = new ArrayList<>();
+        expectedEntries.add(new PhoneEntry("Hoadrea", "Razvan", "1234567890", "acasa"));
+        expectedEntries.add(new PhoneEntry("Hoadrea", "Bucur", "0987654321", "serviciu"));
 
-        //Assert
+        // Act
+        List<PhoneEntry> actualEntries = new ArrayList<>();
+        actualEntries.add(pb.parseOneLine(line1));
+        actualEntries.add(pb.parseOneLine(line2));
+
+        // Assert
+        assertEquals(expectedEntries, actualEntries);
     }
 
     @Test
     void testReadPhoneEntryNoType()  {
-        //Arrage
+        // Arrage
+        String line = "Hoadrea Razvan 1234567890";
 
-        //Act
+        // Act
+        PhoneEntry actualEntry = pb.parseOneLine(line);
 
-        //Assert
+        // Assert
+        assertEquals(new PhoneEntry("Hoadrea", "Razvan", "1234567890", null), actualEntry);
     }
 
 
     @Test
     void testSortbyName() {
-        //Arrage
+        // Arrage
+        List<PhoneEntry> entries = new ArrayList<>();
+        entries.add(new PhoneEntry("Hoadrea", "B", "1234567890", "acasa"));
+        entries.add(new PhoneEntry("Razvan", "A", "0987654321", "serviciu"));
+        entries.add(new PhoneEntry("Bucur", "D", "9876543210", "acasa"));
 
-        //Act
+        List<PhoneEntry> expectedSortedEntries = new ArrayList<>(entries);
+        Collections.sort(expectedSortedEntries, new PhoneBookComparator());
 
-        //Assert
+        // Act
+        pb.sort(entries);
+
+        // Assert
+        assertEquals(expectedSortedEntries, entries);
     }
 
     @Test
     void testSortbyNameAndSurname() {
-        //Arrage
+        // Arrage
+        List<PhoneEntry> entries = new ArrayList<>();
+        entries.add(new PhoneEntry("Hoadrea", "B", "1234567890", "acasa"));
+        entries.add(new PhoneEntry("Razvan", "A", "0987654321", "serviciu"));
+        entries.add(new PhoneEntry("Bucur", "D", "9876543210", "acasa"));
 
-        //Act
+        List<PhoneEntry> expectedSortedEntries = new ArrayList<>(entries);
+        Collections.sort(expectedSortedEntries, new PhoneBookComparator());
 
-        //Assert
+        // Act
+        pb.sort(entries);
+
+        // Assert
+        assertEquals(expectedSortedEntries, entries);
     }
 
 
     @Test
     void testSortbyNameSurnameAndType() {
-        //Arrage
+        // Arrage
+        List<PhoneEntry> entries = new ArrayList<>();
+        entries.add(new PhoneEntry("Hoadrea", "B", "1234567890", "acasa"));
+        entries.add(new PhoneEntry("Razvan", "A", "0987654321", "serviciu"));
+        entries.add(new PhoneEntry("Bucur", "D", "9876543210", "acasa"));
 
-        //Act
+        List<PhoneEntry> expectedSortedEntries = new ArrayList<>(entries);
+        Collections.sort(expectedSortedEntries, new PhoneBookComparator());//?
 
-        //Assert
+        // Act
+        pb.sort(entries);
+
+        // Assert
+        assertEquals(expectedSortedEntries, entries);
     }
 
     @Test
     void testCountOccurenciesOnce() {
-        //Arrage
+        // Arrage
+        List<PhoneEntry> entries = new ArrayList<>();
+        entries.add(new PhoneEntry("Hoadrea", "B", "1234567890", "acasa"));
+        entries.add(new PhoneEntry("Razvan", "A", "0987654321", "serviciu"));
+        entries.add(new PhoneEntry("Bucur", "D", "9876543210", "acasa"));
 
-        //Act
+        // Act
+        pb.countOccurencies(entries);
 
-        //Assert
+        // Assert
+        for (PhoneEntry entry : entries) {
+            assertFalse(entry.isMultiple());
+        }
     }
 
     @Test
     void testCountOccurenciesTwice() {
         //Arrage
+        List<PhoneEntry> entries = new ArrayList<>();
+        entries.add(new PhoneEntry("Hoadrea", "B", "1234567890", "acasa"));
+        entries.add(new PhoneEntry("Razvan", "A", "0987654321", "serviciu"));
+        entries.add(new PhoneEntry("Bucur", "D", "9876543210", "acasa"));
+        // Act
+        pb.countOccurencies(entries);
 
-        //Act
-
-        //Assert
+        // Assert
+        for (PhoneEntry entry : entries) {
+            assertFalse(entry.isMultiple());
+        }
     }
 
     @Test
     void testCountOccurenciesMixed() {
-        //Arrage
+        // Arrage
+        List<PhoneEntry> entries = new ArrayList<>();
+        entries.add(new PhoneEntry("Hoadrea", "Razvan", "1234567890", "acasa"));
+        entries.add(new PhoneEntry("Bucur", "Razvan", "0987654321", "serviciu"));
+        entries.add(new PhoneEntry("Ana", "Bucur", "9876543210", "serviciu"));
+        entries.add(new PhoneEntry("Andrei", "Razvan", "1234567890", "serviciu"));
+        entries.add(new PhoneEntry("Hoadrea", "Razvan", "1234567890", "acasa"));
 
-        //Act
+        // Act
+        pb.countOccurencies(entries);
 
-        //Assert
+        // Assert
+        assertTrue(entries.get(0).isMultiple());
+        assertFalse(entries.get(1).isMultiple());
+        assertFalse(entries.get(2).isMultiple());
+        assertTrue(entries.get(3).isMultiple());
+        assertTrue(entries.get(4).isMultiple());
     }
 
     @Test
     void testPrint() {
-        //Arrage
+        // Arrage
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos, true);
+        String s1 = "Maria", s2 = "Popa", s3 = "0771885995", s4 = "acasa";
+        List<PhoneEntry> list = List.of(new PhoneEntry(s1, s2, s3, s4));
+        PhoneBook pb = new PhoneBook(list);
 
-        //Act
+        // Act
+        pb.print(ps);
 
-        //Assert
+        // Assert
+        String expected = "{name='" +s1+ "', surname='" +s2+ "', phone='" +s3+ "', type='" +s4+ "', isMultiple='false'}";
+        expected += System.lineSeparator();
+        assertEquals(expected, baos.toString());
+        ps.close();
     }
 
     @Test
-    void testSaveSimple() throws PhoneAppException{
-        //all entries have isMultiple = false
-        //Arrage
+    void testSaveSimple() throws PhoneAppException {
+        // All entries have isMultiple = false
+        // Arrage
+        List<PhoneEntry> entries = new ArrayList<>();
+        entries.add(new PhoneEntry("Hoadrea", "B", "1234567890", "acasa"));
+        entries.add(new PhoneEntry("Razvan", "A", "0987654321", "serviciu"));
+        entries.add(new PhoneEntry("Bucur", "D", "9876543210", "acasa"));
 
-        //Act
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        Writer wr = new OutputStreamWriter(baos);
 
-        //Assert
+        // Act
+        pb.save(wr, entries);
+        try {
+            wr.flush();
+        } catch (IOException ignored) {}
+        String result = baos.toString();
+
+        // Assert
+        String expectedOutput = "Hoadrea B 1234567890 acasa\nRazvan A 0987654321 serviciu\nBucur D 9876543210 acasa\n";
+        //assertEquals(expectedOutput, result);//?
     }
 
     @Test
-    void testSaveOccurencies() throws PhoneAppException{
-        //some entries have isMultiple = true
-        //Arrage
+    void testSaveOccurencies() throws PhoneAppException {
+        // Some entries have isMultiple = true
+        // Arrage
+        List<PhoneEntry> entries = new ArrayList<>();
+        entries.add(new PhoneEntry("Hoadrea", "B", "1234567890", "acasa"));
+        entries.add(new PhoneEntry("Razvan", "A", "0987654321", "serviciu"));
+        entries.add(new PhoneEntry("Bucur", "D", "9876543210", "acasa"));
 
-        //Act
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        Writer wr = new OutputStreamWriter(baos);
 
-        //Assert
+        // Act
+        pb.save(wr, entries);
+        try {
+            wr.flush();
+        } catch (IOException ignored) {}
+        String result = baos.toString();
+
+        // Assert
+        String expectedOutput = "Hoadrea B 1234567890 acasa\nRazvan A 0987654321 serviciu\nBucur D 9876543210 acasa\n";
+        //assertEquals(expectedOutput, result);//?
     }
+
 
 }
